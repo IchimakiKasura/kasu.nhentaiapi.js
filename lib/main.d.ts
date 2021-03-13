7938a15853e002b66e64a160c993d402742550e1
IchimakiKasura <80595346+IchimakiKasura@users.noreply.github.com>
2021-03-14 01:46:03 +0800

    Add files via upload

---
+++ lib/main.d.ts
@@ -0,0 +1,37 @@
+export = main;
+declare class main {
+    getID(ID:string|number): {
+        list: (data?: {}) => Promise<any>;
+        json: () => Promise<{
+            base: {
+                id: number;
+                url: string;
+                title: {
+                    origin: string;
+                    translated: string;
+                };
+                images: {
+                    cover: string;
+                    pages: string;
+                };
+                tag_table: {
+                    parodies: string;
+                    characters: string;
+                    tag: string;
+                    artist: string;
+                    groups: string;
+                    languages: string;
+                    categories: string;
+                };
+                number_pages: string;
+                uploaded: string;
+            };
+        }>;
+    };
+    pRandSpecificTags(string?: string, data?: {}): Promise<any>;
+    pRandTag(string?: string, data?: {}): Promise<any>;
+    pRandParody(string?: string, data?: {}): Promise<any>;
+    pRandArtist(string?: string, data?: {}): Promise<any>;
+    pRandGroup(string?: string, data?: {}): Promise<any>;
+    pRandom(data?: {}): Promise<any>;
+}
